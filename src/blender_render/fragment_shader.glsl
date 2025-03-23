fa0a312b44607d25921ef1f1d3622c29e566225f
a-johanson <a-johanson@users.noreply.github.com>
2025-03-23 20:47:16 +0100

    remove redundant computation for surface orientation

--- src/blender_render/fragment_shader.glsl
+++ src/blender_render/fragment_shader.glsl
@@ -1,17 +1,13 @@
 void main() {
-    float depth = length(pos);
-
     vec3 lightDirection = normalize(lightPosition - pos);
     vec3 normal = normalize(norm);
     float normal_amount = dot(normal, lightDirection);
     float diffuse = max(normal_amount, 0.0f);
 
-    vec3 u = normalize(lightDirection - normal_amount * normal);
-    const float eps = 1.0e-6f;
-    vec3 b = pos + eps * u;
-    vec3 a = pos - eps * u;
-    vec2 dir = b.xy - a.xy;
-    float orientation = atan(dir.y, dir.x);
+    vec3 u = lightDirection - normal_amount * normal;
+    float orientation = atan(u.y, u.x);
+
+    float depth = length(pos);
 
     FragColor = vec4(diffuse, orientation, depth, 1.0f);
 }
